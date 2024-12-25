f200326206eb3c68d729866af12a7b042ae82e6a
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-12-24 19:00:45 -0500

    feat: add Vercel adapter for deployment support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import svelte from '@astrojs/svelte';
-
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
+  adapter: vercel(),
 });
